e6feafc8cc77d07a6af84e6df6c9d1d32c3c3f43
Enjie <angelmuhaeni@gmail.com>
2026-05-21 22:44:16 +0700

    fix(response): add timestamp fields to all responses

---
+++ src/utils/normalize.js
@@ -0,0 +1,12 @@
+export const normalizeApplication = (app) => {
+  return {
+    ...app,
+    status: typeof app.status === "string" 
+      ? app.status.replace(/^["']|["']$/g, "") 
+      : app.status,
+  };
+};
+
+export const normalizeApplications = (apps) => {
+  return apps.map((app) => normalizeApplication(app));
+};
