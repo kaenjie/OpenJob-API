0ee20bb8c0b6aaddceb35c7581c6220f570698a6
Enjie <angelmuhaeni@gmail.com>
2026-05-21 22:44:16 +0700

    fix(errors): add NotFoundError handling in middleware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,30 +1,44 @@
 import ClientError from "../exceptions/ClientError.js";
+import NotFoundError from "../exceptions/NotFoundError.js";
+import { sendResponse } from "../utils/response.js";
 
 const errorMiddleware = (err, req, res, next) => {
-  if (err instanceof ClientError) {
-    return res.status(err.statusCode).json({
+  if (err instanceof NotFoundError) {
+    return sendResponse(res, {
       status: "failed",
+      statusCode: 404,
+      message: err.message,
+    });
+  }
+
+  if (err instanceof ClientError) {
+    return sendResponse(res, {
+      status: "failed",
+      statusCode: err.statusCode,
       message: err.message,
     });
   }
 
   if (err.code === "23503") {
-    return res.status(400).json({
+    return sendResponse(res, {
       status: "failed",
+      statusCode: 400,
       message: "Data referensi tidak ditemukan",
     });
   }
 
   if (err.code === "23505") {
-    return res.status(400).json({
+    return sendResponse(res, {
       status: "failed",
+      statusCode: 400,
       message: "Data sudah ada",
     });
   }
 
   console.error(err);
-  return res.status(500).json({
+  return sendResponse(res, {
     status: "error",
+    statusCode: 500,
     message: "Terjadi kesalahan pada server",
   });
 };
